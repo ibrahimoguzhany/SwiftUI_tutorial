f0c13223c3bbe09c6d149d5db91f7ca0f32ee009
ibrahimoguzhany <ibrahimoguzhany@gmail.com>
2021-10-02 23:27:01 +0300

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,10 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        sampleHorizontalView()
     }
-}
+
+    fileprivate func sampleHorizontalView() -> some View {
+        return HStack{
+            Spacer().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            Text("oguzhan")
+                .padding()
+            Spacer()
+            Text("oguzhan")
+                .padding()
+            Spacer()
+            Text("oguzhan")
+                .padding()
+            Color.red
+            Spacer().frame(height: 050, alignment: .center
+            ).frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            
+        }
+    }
+    
+    }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
