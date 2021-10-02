5fe706e4370ba528bbd8feabe86b9935b1978c47
ibrahimoguzhany <ibrahimoguzhany@gmail.com>
2021-10-02 22:57:23 +0300

    Initial Commit

---
+++ Shared/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  Shared
+//
+//  Created by Oğuzhan Yılmaz on 2.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
